5f6f8b50d968d03cc32eead7bc623003878e58d4
yangyang.cyy <yangyang.cyy@alibaba-inc.com>
2021-10-31 12:15:44 +0800

    unused

--- quarkus-mybatis-deployment/src/test/java/io/quarkiverse/mybatis/test/TestResources.java
+++
@@ -1,9 +0,0 @@
-package io.quarkiverse.mybatis.test;
-
-import io.quarkus.test.common.QuarkusTestResource;
-import io.quarkus.test.h2.H2DatabaseTestResource;
-
-@QuarkusTestResource(H2DatabaseTestResource.class)
-//@QuarkusTestResource(DerbyDatabaseTestResource.class)
-public class TestResources {
-}
