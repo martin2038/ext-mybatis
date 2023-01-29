08ba709353401c17861f14fd3d4793a355d75438
Martin.Cong <martin.cong@bestulearn.com>
2023-01-29 11:47:53 +0800

    refresh

--- refresh.sh
+++ refresh.sh
@@ -1,1 +1,5 @@
-gradle clean publish && rm -rf ~/.gradle/caches/modules-2/files-2.1/com.bt.common/*  && rm -rf ~/.gradle/caches/modules-2/files-2.1/com.bt.ext/* 
+rm -rf ~/.gradle/caches/modules-2/files-2.*/com.bt*
+rm -rf ~/.gradle/caches/modules-2/metadata-2.*/descriptors/com.bt*
+
+gradle clean publish
+#&& rm -rf ~/.gradle/caches/modules-2/files-2.1/com.bt.common/*  && rm -rf ~/.gradle/caches/modules-2/files-2.1/com.bt.ext/*
